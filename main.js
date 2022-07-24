a48ee369cb5f8cfd2348e81ab95cd5f93008d4b0
Rasla <programmer.rasla@gmail.com>
2022-07-24 20:53:02 +0530

    new changes

--- main.js
+++ main.js
@@ -34,16 +34,6 @@ let links = [
 
 // create list of links
 (() => {
-  document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-      document.querySelector("body").style.visibility = "hidden";
-      document.querySelector("#loader").style.visibility = "visible";
-    } else {
-      document.querySelector("#loader").style.display = "none";
-      document.querySelector("body").style.visibility = "visible";
-    }
-  };
-
   createLinks(links);
   if (mobileCheck()) {
     changeIconColors();
