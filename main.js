371e043eedafeacf5f2d9d7358a3ab8138b972c8
Rasla <programmer.rasla@gmail.com>
2022-07-26 08:51:47 +0530

    finded a new way

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import "./style.css";
-import "./utils/utils.js";
-import { mobileCheck } from "./utils/utils.js";
+import { videoData } from "./utils/utils.js";
 
 let color = "ffffff";
 
@@ -34,27 +33,92 @@ let links = [
 
 // create list of links
 (() => {
+  const video = document.getElementById("vid");
+  video.src = videoData;
+  video.autoplay = true;
+  video.loop = true;
+  video.muted = true;
+  video.playbackRate = 1;
+  video.controls = false;
+  video.setAttribute("playsinline", "");
+  video.setAttribute("webkit-playsinline", "");
+
   createLinks(links);
-  if (mobileCheck()) {
-    changeIconColors();
-  }
-
-  // make video mute
-  const video = document.querySelector("video");
-  const mute = document.querySelector(".app .mute-btn");
-  const muteBtn = document.querySelector(".app .mute-btn img");
-
-  muteBtn.src = "https://img.icons8.com/ios/30/ffffff/mute--v1.png";
-
-  video.volume = 0.4;
-  mute.addEventListener("click", () => {
-    video.muted = !video.muted;
-    if (video.muted) {
-      muteBtn.src = "https://img.icons8.com/ios/30/ffffff/mute--v2.png";
-    } else {
-      muteBtn.src = "https://img.icons8.com/ios/30/ffffff/mute--v1.png";
-    }
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 400, density: { enable: true, value_area: 3000 } },
+      color: { value: "#fc0000" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 3 },
+        image: { src: "img/github.svg", width: 100, height: 100 },
+      },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: true, speed: 5, size_min: 0, sync: false },
+      },
+      line_linked: {
+        enable: false,
+        distance: 500,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 2,
+      },
+      move: {
+        enable: true,
+        speed: 7.8914764163227265,
+        direction: "top",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: false, mode: "bubble" },
+        onclick: { enable: false, mode: "repulse" },
+        resize: true,
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 0.5 } },
+        bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
+      },
+    },
+    retina_detect: true,
   });
+  var count_particles, stats, update;
+  stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.left = "0px";
+  stats.domElement.style.top = "0px";
+  document.body.appendChild(stats.domElement);
+  count_particles = document.querySelector(".js-count-particles");
+  update = function () {
+    stats.begin();
+    stats.end();
+    if (
+      window.pJSDom[0].pJS.particles &&
+      window.pJSDom[0].pJS.particles.array
+    ) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
 })();
 
 function createLinks(links) {
